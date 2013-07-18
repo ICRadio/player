ccd90e91d09c8021a007409d41cbc846b3f8468a
Joe Letts <entrepreneurj@gmail.com>
2013-07-18 19:48:24 +0100

    Initial draft of plugin

--- README.md
+++ README.md
@@ -1,4 +1,20 @@
-plugins
+player
 =================
 
-Plugins that can be embedded on external websites
+A live stream plugin that can be embedded on external websites
+
+In order to use the plugin:
+1. Add references to jQuery and the plugin script  to your webpage. 
+e.g. <head>
+ <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+ <script type="text/javascript" src="http://members.icradio.com/plugins/player/plugin.js"></script>
+</head>
+
+2. Add the following code to the area of the website where the player will reside:
+<div id="icradio-live-plugin"></div>
+
+3. If you would like to customise the player create custom css based on the /plugin.css fle.
+
+Enjoy!
+
+IC Radio Tech
---
+++ plugin.js
@@ -0,0 +1,27 @@
+
+
+
+function loadjscssfile(filename, filetype){
+ if (filetype=="js"){ //if filename is a external JavaScript file
+  var fileref=document.createElement('script')
+  fileref.setAttribute("type","text/javascript")
+  fileref.setAttribute("src", filename)
+ }
+ else if (filetype=="css"){ //if filename is an external CSS file
+  var fileref=document.createElement("link")
+  fileref.setAttribute("rel", "stylesheet")
+  fileref.setAttribute("type", "text/css")
+  fileref.setAttribute("href", filename)
+ }
+ if (typeof fileref!="undefined")
+  document.getElementsByTagName("head")[0].appendChild(fileref)
+}
+
+loadjscssfile("http://members.icradio.com/plugins/player/plugin.css", "css") //dynamically load and add this .css file
+
+$(function () {
+// To add source element for ogg
+$("#icradio-live-plugin").html("<audio controls=\"controls\"><source src=\"http:\/\/icecast.icradio.com:8000\/live-mp3-high\" type=\"audio\/mpeg\" />Sorry, your browser doesn't support HTML5 audio.</audio>")
+
+
+});
